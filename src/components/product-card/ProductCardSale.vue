3f97fe82fe7d60de8cd1d3d26c93b4771f18748c
Vadim Ignatov <v.ignatov@idaproject.com>
2023-06-14 21:10:36 +0300

    feat: add sale

---
+++ src/components/product-card/ProductCardSale.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="product-card-sale">
+        Sale -{{ percentage }}%
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ProductCardSale",
+
+    props: {
+        percentage: {
+            type: Number,
+            default: 0,
+        },
+    },
+}
+</script>
+
+<style>
+.product-card-sale {
+    padding: 12px 20px;
+    font-weight: 500;
+    color: #ffffff;
+    background-color: #FF6B00;
+}
+</style>
