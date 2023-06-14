840d32c2379a6dc9f51e0dc9533ae8fd207d9e47
Vadim Ignatov <v.ignatov@idaproject.com>
2023-06-14 22:52:58 +0300

    feat: add conditionals for layout

--- src/components/grid/GridLayout.vue
+++ src/components/grid/GridLayout.vue
@@ -1,12 +1,20 @@
 <template>
-    <div class="grid-layout">
-        <div class="grid-layout__present">
+    <div :class="['grid-layout', { 'grid-layout--biggest': $slots.big }]">
+        <div
+            v-if="$slots.present"
+            :class="['grid-layout__present', { 'grid-layout__present--big': $slots.big }]"
+        >
             <slot name="present" />
         </div>
 
-        <div class="grid-layout__big-element">
+        <div
+            v-if="$slots.big"
+            class="grid-layout__big-element"
+        >
             <slot name="big" />
         </div>
+
+        <slot />
     </div>
 </template>
 
@@ -27,9 +35,19 @@ export default {
 .grid-layout {
     display: grid;
     grid-template-columns: repeat(4, 1fr);
+    grid-column-gap: 20px;
+    grid-row-gap: 60px;
+}
+
+.grid-layout--biggest {
+    grid-template-rows: 380px auto;
 }
 
 .grid-layout__present {
+    grid-area: 1 / span 4
+}
+
+.grid-layout__present--big {
     grid-area: 1 / 1 / 2 / 3;
 }
 
