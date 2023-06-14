9c28994eb181bdee489089d7b93c52f9ca3519e9
Vadim Ignatov <v.ignatov@idaproject.com>
2023-06-14 22:16:17 +0300

    feat: set filtered logic

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -2,17 +2,36 @@
     <main class="main-page">
         <div class="container">
             <TheFilter
-                v-model="filter"
                 class="main-page__filter"
+                @apply="getProduct"
             />
 
-            <GridLayout>
-                <template #present>
-                    <div class="main-page__count">
-                        Bestsellers:
-                    </div>
-                </template>
-            </GridLayout>
+            <transition name="fade" mode="out-in">
+                <GridLayout v-if="products && products.length">
+                    <template #present>
+                        <div class="main-page__count">
+                            Bestsellers:
+                        </div>
+                    </template>
+
+                    <template #big>
+                        <ProductCard
+                            v-if="bigCardProduct"
+                            v-bind="bigCardProduct"
+                        />
+                    </template>
+
+                    <ProductCard
+                        v-for="item in products"
+                        :key="item.id"
+                        v-bind="item"
+                    />
+                </GridLayout>
+
+                <h2 v-else>
+                    No results
+                </h2>
+            </transition>
         </div>
     </main>
 </template>
@@ -20,20 +39,44 @@
 <script>
 import TheFilter from "@/components/filter/TheFilter";
 import GridLayout from "@/components/grid/GridLayout";
+import ProductCard from "@/components/product-card/ProductCard";
 
 export default {
     name: "MainPage",
 
     components: {
         TheFilter,
-        GridLayout
+        GridLayout,
+        ProductCard
     },
+
+    data() {
+        return {
+            products: [],
+        }
+    },
+
+    computed: {
+        bigCardProduct() {
+            return this.products.find(item => item.bigCard)
+        }
+    },
+
+    methods: {
+        async getProduct(filterValues = {}) {
+            try {
+                this.products = await this.$store.dispatch('getProducts', filterValues);
+            } catch (e) {
+                console.log(e)
+            }
+        },
+    }
 }
 </script>
 
 <style>
 .main-page {
-    padding-top: 80px;
+    padding: 80px 0 120px;
 }
 
 .main-page__filter {
@@ -43,6 +86,7 @@ export default {
 .main-page__count {
     text-transform: uppercase;
     font-size: 40px;
+    line-height: 48px;
     font-weight: 700;
 }
 </style>
