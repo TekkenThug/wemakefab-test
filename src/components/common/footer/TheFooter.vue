b86e8e7592dc83057baff5489cc3311c10e8ce2f
Vadim Ignatov <ignatov0131@gmail.com>
2023-06-15 09:01:46 +0300

    feat: make footer

---
+++ src/components/common/footer/TheFooter.vue
@@ -0,0 +1,60 @@
+<template>
+    <footer class="footer">
+        <div class="container">
+            <div class="footer__wrapper">
+                <TheLogo theme="white" />
+
+                <ul class="footer__contact-list">
+                    <li class="footer__contact-item">
+                        <a href="/copyright">Copyright</a>
+                    </li>
+
+                    <li class="footer__contact-item">
+                        <a href="/contacts">Contacts</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+import TheLogo from "@/components/common/logo/TheLogo";
+
+export default {
+    name: "TheFooter",
+
+    components: {
+        TheLogo,
+    },
+}
+</script>
+
+<style>
+.footer {
+    padding: 40px 0 41px;
+    background-color: #1B1B1B;
+}
+
+.footer__wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.footer__contact-list {
+    display: flex;
+    align-items: center;
+}
+
+.footer__contact-item {
+    color: #ffffff;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 22px;
+}
+
+.footer__contact-item:not(:last-child) {
+    margin-right: 40px;
+}
+</style>
