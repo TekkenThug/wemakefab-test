e51a2c9c63cee505ab6ef49976aead522010dc3c
Vadim Ignatov <v.ignatov@idaproject.com>
2023-06-15 08:59:04 +0300

    feat: create logo component

---
+++ src/components/common/logo/TheLogo.vue
@@ -0,0 +1,36 @@
+<template>
+    <a :class="['logo', `logo--${theme}`]" href="/">
+        S—Shop
+    </a>
+</template>
+
+<script>
+export default {
+    name: "TheLogo",
+
+    props: {
+        theme: {
+            type: String,
+            default: 'dark',
+            validator: v => ['dark', 'white'].includes(v)
+        }
+    }
+}
+</script>
+
+<style>
+.logo {
+    text-transform: uppercase;
+    font-size: 24px;
+    font-weight: 600;
+    line-height: 29px;
+}
+
+.logo--dark {
+    color: #000000;
+}
+
+.logo--white {
+    color: #ffffff;
+}
+</style>
